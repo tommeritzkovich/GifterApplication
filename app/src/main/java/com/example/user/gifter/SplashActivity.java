212a46caca351a3d88c5894b6fac46beb78f5b13
Tomer <tomimi004>
2019-02-15 17:05:47 +0200

    Added Home and Splash pages

---
+++ app/src/main/java/com/example/user/gifter/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.user.gifter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    Intent homeActivity;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        //Inits
+        homeActivity = new Intent(this, HomeActivity.class);
+    }
+
+    public void splashButton(View view) {
+        startActivity(homeActivity);
+    }
+}
